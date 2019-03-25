5098c62ecae4a4034982b565db85b2383e6f3088
PritomKumar <bsse0919@iit.du.ac.bd>
2019-03-26 04:04:22 +0600

    other deparment

--- FirebaseAuthentication/app/src/main/java/com/example/ohno/AccountActivity.java
+++ FirebaseAuthentication/app/src/main/java/com/example/ohno/AccountActivity.java
@@ -156,15 +156,105 @@ public class AccountActivity extends AppCompatActivity {
 
     public void departmentAddData() {
         String [] temp = {"Faculty of Arts","Department of Bangla", "Department of English",
-                "Department of Persian Language and Literature"," Department of Philosophy",
+                "Department of Persian Language and Literature","Department of Philosophy",
                 "Department of History", "Department of Arabic","Department of Islamic Study",
-                "Department of Islamic History and Culture"," Department of Sanskrit and Pali",
+                "Department of Islamic History and Culture","Department of Sanskrit and Pali",
                 "Department of Information Science and Library Management",
                 "Department of Languages and Linguistics", "Department of Theater and Music",
-                "Department of World Religion and Culture"};
+                "Department of World Religion and Culture"
+        };
         List<String> arts = new ArrayList<String>(Arrays.asList(temp));
 
-        String [] temp2 = {"Faculty Of Arts","one"};
+        String [] temp2 = {"Faculty of Business Studies","Department of Management Studies",
+                "Department of Accounting & Information Systems", "Department of Marketing",
+                "Department of Finance", "Department of Banking and Insurance",
+                "Department of Management Information Systems", "Department of International Business",
+                "Department of Tourism and Hospitality Management",
+                "Department of Organization Strategy and Leadership"
+        };
+        List<String> bussiness = new ArrayList<String>(Arrays.asList(temp2));
+
+        String [] temp3 = {"Faculty of Biological Science","Department of Soil, Water and Environment",
+                "Department of Botany", "Department of Zoology",
+                "Department of Biochemistry and Molecular Biology", "Department of Psychology",
+                "Department of Microbiology", "Department of Fisheries Science",
+                "Department of Medical Psychology",
+                "Department of Genetic Engineering and Biotechnology",
+                "Department of Educational Psychology"
+        };
+        List<String> biology = new ArrayList<String>(Arrays.asList(temp3));
+
+        String [] temp4 = {"Faculty of Engineering and Technology",
+                "Department of Electrical and Electronic Engineering",
+                "Department of Applied Chemistry and Chemical Engineering",
+                "Department of Computer Science and Engineering",
+                "Department of Nuclear Engineering", "Department of Robotics and Mechatronics"
+        };
+        List<String> engineering = new ArrayList<String>(Arrays.asList(temp4));
+
+        String [] temp5 = {"Faculty of Fine Arts","Department of Drawing and Painting",
+                "Department of Graphic Design",
+                "Department of Printmaking", "Department of Oriental Art",
+                "Department of Ceramics", "Department of Sculpture",
+                "Department of Craft", "Department of History of Art"
+        };
+        List<String>  fineArts  = new ArrayList<String>(Arrays.asList(temp5));
+
+        String [] temp6 = {"Faculty of Law","Department of Law"};
+        List<String> law = new ArrayList<String>(Arrays.asList(temp6));
+
+        String [] temp7 = {"Faculty of Medicine","Bachelor of physiotherapy(BPT)"};
+        List<String> medicine = new ArrayList<String>(Arrays.asList(temp7));
+
+        String [] temp8 = {"Faculty of Pharmacy","Department of Pharmaceutical Chemistry",
+                "Department of Clinical Pharmacy and Pharmacology",
+                "Department of Pharmaceutical Technology", "Department of Pharmacy"
+        };
+        List<String> pharmacy = new ArrayList<String>(Arrays.asList(temp8));
+
+        String [] temp9 = {"Faculty of Science","Department of Physics",
+                "Department of Mathematics", "Department of Chemistry",
+                "Department of Biomedical Physics and Technology", "Department of Theoretical Physics",
+                "Department of Applied Mathematics", "Department of Statistics",
+                "Department of Theoretical and Computational Chemistry"
+        };
+        List<String> science = new ArrayList<String>(Arrays.asList(temp9));
+
+        String [] temp10 = {"Faculty of Business Studies","Department of Management Studies",
+                "Department of Accounting & Information Systems", "Department of Marketing",
+                "Department of Finance", "Department of Banking and Insurance",
+                "Department of Management Information Systems", "Department of International Business",
+                "Department of Tourism and Hospitality Management",
+                "Department of Organization Strategy and Leadership",
+        };
+        List<String> bussiness = new ArrayList<String>(Arrays.asList(temp10));
+
+        String [] temp11 = {"Faculty of Social Sciences","Department of Economics",
+                "Department of Health Economics ", "Department of Political Science",
+                "Department of International Relations", "Department of Anthropology",
+                "Department of Public Administration", "Department of Mass Communication and Journalism",
+                "Department of Communication Disorders",
+                "Department of Printing and Publication Studies",
+                "Department of Television, Film and Photography",
+                "Department of Sociology", "Department of Development Studies",
+                "Department of Criminology", "Department of Japanese Studies"
+        };
+        List<String> socialSciences = new ArrayList<String>(Arrays.asList(temp11));
+
+        String [] temp12 = {"Faculty of Earth and Environmental Sciences",
+                "Department of geography and environment", "Department of Geology",
+                "Department of oceanography",
+                "Department of Disaster Science and Management", "Department of Meteorology"
+        };
+        List<String> earthEnvironmentalSciences = new ArrayList<String>(Arrays.asList(temp12));
+
+        String [] temp2 = {"Faculty of Business Studies", "Department of Management Studies",
+                "Department of Accounting & Information Systems", "Department of Marketing",
+                "Department of Finance", "Department of Banking and Insurance",
+                "Department of Management Information Systems", "Department of International Business",
+                "Department of Tourism and Hospitality Management",
+                "Department of Organization Strategy and Leadership",
+        };
         List<String> bussiness = new ArrayList<String>(Arrays.asList(temp2));
 
 
